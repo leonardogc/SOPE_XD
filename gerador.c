146cacad3577ba3b7240163c0bee37d8ecbf682a
leonardogc <leonardogomescapozzi@gmail.com>
2017-05-14 19:52:14 +0100

    Add files via upload

--- gerador.c
+++ gerador.c
@@ -6,8 +6,17 @@
 #include <time.h>
 #include <stdlib.h>
 
-int numPedidos=10;
-int maxUtilizacao=10000;
+unsigned long numPedidos;
+unsigned long maxUtilizacao;
+
+int writeFIFO;
+int readFIFO;
+
+char * PATH_REQUEST_QUEUE = "/tmp/entrada";
+char * PATH_REJECTED_QUEUE = "/tmp/rejeitados";
+
+pthread_mutex_t registry_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 FILE * file;
 
 /*
@@ -17,17 +26,27 @@ ssize_t write (int fd, char * buf, int count);
 
 void * listenerThread(void * arg){
 	int fd;
-	int n;
+	int counter = 0;
 	char str[100];
 
-	//fd= open (/*const char *filename*/, O_RDONLY);
+	while(counter<numPedidos){
 
-	/*do{
 
-	n = read(fd,str,1);
+
+
+
+
+
+
+
+
+
+
 
 	}
-	while (n>0 && *(str++) != '\0');*/
+
+	close(writeFIFO);
+	close(readFIFO);
 
 
  return NULL;
@@ -41,11 +60,11 @@ void * geraPedidos(void * arg){
 	int messageLength;
 	char message[100];
 	char gender;
+	int counter=0;
 
 
-   //  fd= open (/*const char *filename*/,O_WRONLY);
-
-	/*falta o loop*/
+	while(counter<numPedidos)
+	{
 
 	serial++;
 
@@ -58,25 +77,48 @@ void * geraPedidos(void * arg){
 
 	time= (rand() % maxUtilizacao)+1;
 
-    sprintf(message, "%d-%c-%d-0/", serial,gender,time);
+	messageLength= sprintf(message, "%d-%c-%d-0/", serial,gender,time);
 
-    messageLength=strlen(message)+1;
+    write(writeFIFO,message,messageLength);
 
-    write(fd,message,messageLength);
+    pthread_mutex_lock(&registry_mutex);
 
     fprintf(file,"%-10.2f – %-10d – %-10d: %-1c – %-10d – %-15s\n", inst, getpid(), serial , gender, time,"PEDIDO");
 
+    pthread_mutex_unlock(&registry_mutex);
+
+    counter++;
+	}
 
  return NULL;
 }
 
 
-int main(){
+int main(int argc, char ** argv){
+
+	if (argc != 3)
+	    {
+	        fprintf(stdout, "Usage: ./sauna <no. of seats>\n");
+	        return 0;
+	    }
+
+	numPedidos = strtoul(argv[1], NULL, 10);
+	maxUtilizacao = strtoul(argv[2], NULL, 10);
+
+	    if (numPedidos == 0 || numPedidos == ULONG_MAX || maxUtilizacao == 0 || maxUtilizacao == ULONG_MAX)
+	    {
+	        fprintf(stderr, "Invalid argument! Must be an integer greater than 0 and lesser than");
+	        return 1; // Runtime error - user failure
+	    }
+
 	char* filename;
 	sprintf(filename,"ger.%d",getpid());
 	file=fopen(filename,"w");
 
 	pthread_t ta, tb;
+
+	readFIFO= open (PATH_REJECTED_QUEUE, O_RDONLY);
+	writeFIFO= open (PATH_REQUEST_QUEUE,O_WRONLY);
 
 	pthread_create(&ta, NULL, geraPedidos, NULL);
 	pthread_create(&tb, NULL, listenerThread, NULL);
